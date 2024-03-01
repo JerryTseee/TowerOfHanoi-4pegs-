727fe8b9d26bfb624db55d49239bf213abfe5d95
TseWangPok_Jerry <126223772+JerryTseee@users.noreply.github.com>
2024-03-01 10:59:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+counter = 0 #to count the total number of movements
 def toh(n, src, aux1, aux2, dest):
+    global counter #access the global variable
     if n==0:
         return
     elif n==1:
+        counter+=1
         print(f"move disk {n} from {src} to {dest}")
         return
     else:
@@ -10,16 +13,20 @@ def toh(n, src, aux1, aux2, dest):
 
         #then move the (n-1)th tower to the aux1
         print(f"move disk {n-1} from {src} to {aux1}")
+        counter+=1
 
         #then move the (n)th tower to the dest
         print(f"move disk {n} from {src} to {dest}")
+        counter+=1
 
         #then move the (n-1)th tower to the dest
         print(f"move disk {n-1} from {aux1} to {dest}")
+        counter+=1
 
         #then move the n-2 towers to the dest by using aux1
         toh(n-2,aux2, aux1, src, dest)
     
     return
 
-toh(5,"peg1","peg2","peg3","peg4")
+toh(7,"peg1","peg2","peg3","peg4")
+print(counter)
